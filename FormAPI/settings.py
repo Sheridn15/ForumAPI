c04cbf81b3777419f7805f3b80b937c1cbc6a95c
unknown <burhoniddinbahredd@gmail.com>
2024-11-22 11:12:13 +0500

    changed settings

--- FormAPI/settings.py
+++ FormAPI/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'FormAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlшеу3',
-        'NAME': BASE_DIR,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         # 'NAME': 'FormAPI',
         # 'USER': 'postgres',
         # 'PASSWORD': '2009',
